33002836c43a7353f1381d9e9fed7e35389c0efe
Jorge Clésio <jorgepebas@gmail.com>
2017-07-13 16:07:35 -0300

    Update cad_comanda.php

--- scripts/cad_comanda.php
+++ scripts/cad_comanda.php
@@ -1,8 +1,6 @@
 <?php
 include '../conexao.php';
-
 	if (isset($_POST['submit'])) {
-
 		$id_comanda   = $_POST['id_comanda'];
 		$cli_nome     = $_POST['nome'];
 		$data         = $_POST['data'];
@@ -12,17 +10,12 @@ include '../conexao.php';
 		$produto   = $_POST['produto'];
 		$qtd_produto  = $_POST['qtd_produto'];
 
-	//$sql_comanda = "INSERT INTO comanda (id_cliente, id_colaborador, data) 
-	 //                VALUES ('$cli_nome','$colaborador','$data')";
-	//$query = mysqli_query($conexao, $sql_comanda);
+$sql_comanda = "INSERT INTO comanda (id_cliente, id_colaborador, data)  VALUES ('$cli_nome','$colaborador','$data')";
+$query = mysqli_query($conexao, $sql_comanda);
 
 	//$sql_servcomanda = "INSERT INTO serv_comanda (id_cliente, id_colaborador, data) 
 	 //                VALUES ('$cli_nome','$colaborador','$data')";
 	//$query = mysqli_query($conexao, $sql_comanda);
-
-	
-
-
 
 	echo $cli_nome .'id do nome <br>'.
 	$id_comanda .'id da comanda <br>'.
@@ -34,7 +27,6 @@ include '../conexao.php';
 	     $qtd_produto.'qtd do produto <br>';
 	  //echo "<script> alert('Cadastro realizado');</script>";
       //echo "<SCRIPT> location.href='../index.php' </SCRIPT>"; 
-
 	//echo "erro ao inserir";
 }
 

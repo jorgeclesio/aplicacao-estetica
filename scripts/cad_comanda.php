71cad69ea785243ffee98a089d789107c41c1a4d
Jorge Clésio <jorgepebas@gmail.com>
2017-07-13 16:09:55 -0300

    Update cad_comanda.php

--- scripts/cad_comanda.php
+++ scripts/cad_comanda.php
@@ -1,21 +1,20 @@
 <?php
 include '../conexao.php';
 	if (isset($_POST['submit'])) {
-		$id_comanda   = $_POST['id_comanda'];
-		$cli_nome     = $_POST['nome'];
-		$data         = $_POST['data'];
-		$servico   = $_POST['servico'];
-		$qtd_servico  = $_POST['qtd_servico'];
-		$colaborador  = $_POST['colaboradores'];
-		$produto   = $_POST['produto'];
-		$qtd_produto  = $_POST['qtd_produto'];
+		$id_comanda	= $_POST['id_comanda'];
+		$cli_nome	= $_POST['nome'];
+		$data		= $_POST['data'];
+		$servico	= $_POST['servico'];
+		$qtd_servico	= $_POST['qtd_servico'];
+		$colaborador	= $_POST['colaboradores'];
+		$produto	= $_POST['produto'];
+		$qtd_produto	= $_POST['qtd_produto'];
 
 $sql_comanda = "INSERT INTO comanda (id_cliente, id_colaborador, data)  VALUES ('$cli_nome','$colaborador','$data')";
 $query = mysqli_query($conexao, $sql_comanda);
 
-	//$sql_servcomanda = "INSERT INTO serv_comanda (id_cliente, id_colaborador, data) 
-	 //                VALUES ('$cli_nome','$colaborador','$data')";
-	//$query = mysqli_query($conexao, $sql_comanda);
+$sql_servcomanda = "INSERT INTO serv_comanda (id_cliente, id_colaborador, data) VALUES ('$cli_nome','$colaborador','$data')";
+$query = mysqli_query($conexao, $sql_comanda);
 
 	echo $cli_nome .'id do nome <br>'.
 	$id_comanda .'id da comanda <br>'.
